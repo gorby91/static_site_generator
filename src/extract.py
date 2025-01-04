ba14dce5911823181d48b53f50b802bba0225631
gorby91 <sam.voller@gmail.com>
2025-01-04 18:19:24 +1100

    improve link extract function by adding negative lookbehind assertion for ''

--- src/extract.py
+++ src/extract.py
@@ -6,5 +6,5 @@ def extract_markdown_images(text):
 
 
 def extract_markdown_links(text):
-    pattern = r"[^!]\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     return re.findall(pattern, text)
