4b4688d5e4db74dc807ee7bc08e0d7f044dcd47c
gorby91 <sam.voller@gmail.com>
2025-01-05 13:31:01 +1100

    refactor blocktoblock with helper functions

--- src/blocktoblocktype.py
+++ src/blocktoblocktype.py
@@ -3,39 +3,57 @@ import re
 from markdowntoblocks import markdown_to_blocks
 
 def block_to_block_type(block: str) -> str:
-    # define a search pattern looking for hashes and match header type
-    if re.search(r"^#{1,6} ", block):
+    if check_header(block):
         return "head"
-    # check start and end for triple backticks to test if code block
-    elif block[:3] == "```" and block[-3:] == "```":
+    if check_code(block):
         return "code"
     # divide lines for following tests to be performed 
     lines = block.splitlines()
+    if check_blockquote(lines):
+        return "blockquote"
+    if check_unordered_list(lines):
+        return "ul"
+    if check_ordered_list(lines):
+        return "ol"
+    else:
+        return "body"
+    
+
+def check_header(block: str) -> bool:
+    # define a search pattern looking for hashes and match header type
+    if re.search(r"^#{1,6} ", block):
+        return "True"
+    return False
+
+def check_code(block: str) -> bool:
+    # check start and end for triple backticks to test if code block
+    return block[:3] == "```" and block[-3:] == "```"
+
+def check_blockquote(lines: list) -> bool:
     # check each line commences with ">" for quoteblock by checking no false values with filter
-    if len(lines) == len(
+    return len(lines) == len(
         list(
             filter(
                 lambda line: line.startswith(">"), lines
             )
         )
-    ):
-        return "blockquote"
+    )
+
+def check_unordered_list(lines: list) -> bool:
     # check each line commences with "* " or "- " for quoteblock by checking no false values with filter
-    elif len(lines) == len(
+    return len(lines) == len(
         list(
             filter(
                 lambda line: line.startswith("* ") or line.startswith("- "), lines
             )
         )
-    ):
-        return "ul"
-    # check if ordered list by looping over lines to check if each line starts with an incrementing value
+    )
+
+def check_ordered_list(lines: list) -> bool:
+     # check if ordered list by looping over lines to check if each line starts with an incrementing value
     for i, line in enumerate(lines):
         startvalue = f"{i + 1}. "
         start_true = line.startswith(startvalue)
         if not start_true:
             break
-    if start_true:
-        return "ol"
-    else:
-        return "body"
+    return start_true
