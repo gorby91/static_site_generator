cef91191dfd935cb59536f6efe8c27269c43745f
gorby91 <sam.voller@gmail.com>
2025-01-05 13:19:01 +1100

    implemented and tested blocktoblocktype

--- src/blocktoblocktype.py
+++ src/blocktoblocktype.py
@@ -3,10 +3,8 @@ import re
 from markdowntoblocks import markdown_to_blocks
 
 def block_to_block_type(block: str) -> str:
-    # define a search pattern looking for hashes and match header type to number of hashes returned
-    header_pattern = r"^(#{1,6}) "
-    header_match = re.search(header_pattern, block)
-    if header_match:
+    # define a search pattern looking for hashes and match header type
+    if re.search(r"^#{1,6} ", block):
         return "head"
     # check start and end for triple backticks to test if code block
     elif block[:3] == "```" and block[-3:] == "```":
@@ -14,21 +12,30 @@ def block_to_block_type(block: str) -> str:
     # divide lines for following tests to be performed 
     lines = block.splitlines()
     # check each line commences with ">" for quoteblock by checking no false values with filter
-    if len(lines) == len(filter(lambda line: line.startswith(">"), lines)):
+    if len(lines) == len(
+        list(
+            filter(
+                lambda line: line.startswith(">"), lines
+            )
+        )
+    ):
         return "blockquote"
     # check each line commences with "* " or "- " for quoteblock by checking no false values with filter
-    elif len(lines) == len(filter(lambda line: line.startswith("* ") or line.startswith("- "), lines)):
+    elif len(lines) == len(
+        list(
+            filter(
+                lambda line: line.startswith("* ") or line.startswith("- "), lines
+            )
+        )
+    ):
         return "ul"
     # check if ordered list by looping over lines to check if each line starts with an incrementing value
     for i, line in enumerate(lines):
-        startvalue = f"{i + 1}."
-        if not line.startswith(startvalue):
-            continue
+        startvalue = f"{i + 1}. "
+        start_true = line.startswith(startvalue)
+        if not start_true:
+            break
+    if start_true:
         return "ol"
     else:
         return "body"
-    
-
-
-test = "#### testing"
-print(block_to_block_type(test))
---
+++ src/test_blocktoblocktype.py
@@ -0,0 +1,75 @@
+import unittest
+
+from blocktoblocktype import block_to_block_type
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_header(self):
+       test_header = ["#### test", "# test", "###### test"]
+       for test in test_header:
+           self.assertEqual(block_to_block_type(test), "head")
+    def test_not_header(self):
+        test_notheader = ["####### test", "##test", "test ##"] 
+        for test in test_notheader:
+            self.assertNotEqual(block_to_block_type(test), "head")
+    def test_code(self):
+        test_code = [codetest1, codetest2]
+        for test in test_code:
+            self.assertEqual(block_to_block_type(test), "code")
+    def test_not_code(self):
+        test_notcode = [codetest3, codetest4]
+        for test in test_notcode:
+            self.assertNotEqual(block_to_block_type(test), "code")
+    def test_quoteblock(self):
+        test_quoteblock = [quotetest1, quotetest2, quotetest3]
+        for test in test_quoteblock:
+            self.assertEqual(block_to_block_type(test), "blockquote")
+    def test_not_quoteblock(self):
+        self.assertNotEqual(block_to_block_type(quotetest4), "blockquote")
+    def test_unordered_list(self):
+        test_ul = [ultest1, ultest2]
+        for test in test_ul:
+            self.assertEqual(block_to_block_type(test), "ul")
+    def test_ordered_list(self):
+        test_ol = [oltest1, oltest2]
+        for test in test_ol:
+            self.assertEqual(block_to_block_type(test), "ol")
+    def test_not_ordered_list(self):
+        self.assertNotEqual(block_to_block_type(oltest3), "ol")
+        
+        
+
+
+
+codetest1 = "```this is code```"
+codetest2 = """```
+this
+is
+also
+code
+```"""
+codetest3 = "```this is not code"
+codetest4 = "``this is also not code```"
+quotetest1 = "> this is a quoteblock"
+quotetest2 = ">this is also a quoteblock"
+quotetest3 = """>as
+>is
+>this"""
+quotetest4 = """> but
+this>
+>isn't"""
+ultest1 = """* I really
+* hate writing
+* in raw html"""
+ultest2 = """* I really
+- hate writing
+- in raw html"""
+oltest1 = """1. This is
+2. an ordered
+3. list"""
+oltest2 = "1. This is an ordered list"
+oltest3 = """1. this is not
+3. an ordered list"""
+
+
+if __name__ == "__main__":
+    unittest.main()
