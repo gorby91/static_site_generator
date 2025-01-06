a53f98531519ecd599d1416f93fc8c918c595d7f
gorby91 <sam.voller@gmail.com>
2025-01-07 09:14:41 +1100

    fix slicing bug in extracttitle.py

--- src/extracttitle.py
+++ src/extracttitle.py
@@ -4,6 +4,6 @@ def extract_title(markdown):
     lines = markdown.splitlines()
     for line in lines:
         if line[:2] == "# ":
-            return line[:2].strip()
+            return line[2:].strip()
     raise Exception("no h1 header found")
     
